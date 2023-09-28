955f8e5e972e4280c9f1b33421c5bd08af990abe
Abilio Gonçalves <128049343+AbilioGV@users.noreply.github.com>
2023-09-28 11:52:18 -0300

    Add files via upload

--- projeto/projeto/ContentView.swift
+++ projeto/projeto/ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     let verdeAmarelo = Color("verdeAmarelo")
     let verdeFundo = Color("verdeFundo")
-    let botao = Color("botao")
+    let marrom = Color("marrom")
     
     var body: some View {
         NavigationView {
@@ -11,68 +11,82 @@ struct ContentView: View {
                 verdeFundo
                     .edgesIgnoringSafeArea(.all)
                 
-                VStack(spacing: 0) {
-                    Rectangle()
-                        .frame(width: 375, height: 120)
-                        .foregroundColor(verdeAmarelo)
-                        .edgesIgnoringSafeArea(.top)
-                    
+                VStack{
+                    VStack {
+                        Rectangle()
+                            .frame(width: .infinity, height: 100)
+                            .foregroundColor(verdeAmarelo)
+                            .shadow(radius: 5)
+                            .overlay(){
+                                Image("Abidle")
+                                    .shadow(radius: 4, x:0, y:4)
+                            }  .ignoresSafeArea()
+                    }
                     Spacer()
                     
                     NavigationLink(destination: paginaJogo()) {
-                        Rectangle()
-                        
-                            .frame(width: 214, height: 43)
-                            .foregroundColor(botao)
-                            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
-                            .padding(.bottom, 20)
+                        ZStack {
+                            Rectangle()
+                                .fill(marrom)
+
+                            Rectangle()
+                                    .strokeBorder(Color(.black), lineWidth: 2)
+                        }
+                        .compositingGroup()
+                        .frame(width: 214, height: 43)
+                        .shadow(radius:4, x:0, y:4)
+                            .padding(.bottom, 10)
                             .overlay(
                                 Text("Jogar")
-                                    .shadow(color: Color.black, radius: 5, x: 0, y: 5)
-                                    .font(.custom("JockeyOne-Regular", size:24))
+                                    .font(.custom("JockeyOne-Regular", size:22))
                                     .foregroundColor(.white)
-                                    .padding(.bottom)
-                            )
+                                    .padding(.bottom))
+                            .padding(.bottom, 10)
                     }
                     
                     NavigationLink(destination: paginaRegras()) {
-                        Rectangle()
-                            .frame(width: 214, height: 43)
-                            .foregroundColor(botao)
-                            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
-                            .padding(.bottom, 20)
+                        ZStack {
+                            Rectangle()
+                                .fill(marrom)
+
+                            Rectangle()
+                                    .strokeBorder(Color(.black), lineWidth: 2)
+                        }
+                        .compositingGroup()
+                        .frame(width: 214, height: 43)
+                        .shadow(radius:4, x:0, y:4)
+                            .padding(.bottom, 10)
                             .overlay(
                                 Text("Regras")
-                                    .shadow(color: Color.black, radius: 5, x: 0, y: 5)
-                                    .font(.custom("JockeyOne-Regular", size:24))
+                                    .font(.custom("JockeyOne-Regular", size:22))
                                     .foregroundColor(.white)
-                                    .padding(.bottom)
-                            )
+                                    .padding(.bottom))
+                            .padding(.bottom, 10)
+                            
                     }
                     
                     NavigationLink(destination: paginaRanking()) {
-                        Rectangle()
-                            .frame(width: 214, height: 43)
-                            .foregroundColor(botao)
-                            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
-                            .padding(.bottom, 20)
+                        ZStack {
+                            Rectangle()
+                                .fill(marrom)
+
+                            Rectangle()
+                                    .strokeBorder(Color(.black), lineWidth: 2)
+                        }
+                        .compositingGroup()
+                        .frame(width: 214, height: 43)
+                        .shadow(radius:4, x:0, y:4)
+                            .padding(.bottom, 10)
                             .overlay(
-                                Text("Estude mais")
-                                    .shadow(color: Color.black, radius: 5, x: 0, y: 5)
-                                    .font(.custom("JockeyOne-Regular", size:24))
+                                Text("Aprenda mais")
+                                    .font(.custom("JockeyOne-Regular", size:22))
                                     .foregroundColor(.white)
-                                    .padding(.bottom)
-                            )
+                                    .padding(.bottom))
+                        
                     }
+                    Image("macacosurf")
+                    .resizable()
                 }
-                .padding(.bottom, 200)
-                
-                Text("Abidle")
-                    .shadow(color: Color.black, radius: 5, x:0 , y: 5)
-                    .foregroundColor(botao)
-                    .font(.custom("JockeyOne-Regular", size:32))
-                    .padding(.bottom, 570)
-                
             }
         }
     }
--- projeto/projeto/paginaJogo.swift
+++ projeto/projeto/paginaJogo.swift
@@ -5,14 +5,14 @@ struct paginaJogo: View {
     @State private var apresentaPopUp = false
     let verdeAmarelo = Color("verdeAmarelo")
     let verdeFundo = Color("verdeFundo")
-    let botao = Color("botao")
-
+    let marrom = Color("marrom")
+    
     var body: some View {
         ZStack{
             verdeFundo
                 .edgesIgnoringSafeArea(.all)
             VStack {
-                    VStack{
+                VStack{
                     
                     Rectangle()
                         .frame(width: .infinity, height: 100)
@@ -21,70 +21,70 @@ struct paginaJogo: View {
                             Text("Abidle")
                                 .font(.custom("JockeyOne-Regular", size: 40))
                                 .fontWeight(.heavy)
-                                .foregroundColor(Marrom)
+                                .foregroundColor(marrom)
                                 .padding(20)
                                 .shadow(radius: 3, x:0, y:5)
-                              
-                        }  .ignoresSafeArea()
                             
-                        }
-                    Spacer()
-                    VStack {
-                        Rectangle()
-                            .frame(width: 230, height:230)
-                        Spacer()
-                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor?")
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .multilineTextAlignment(.center)
-                        Spacer()
-                        Rectangle()
-                            .frame(width: 200, height: 40)
-                            .foregroundColor(Marrom)
-                            .shadow(color: Color.black, radius: 2, x: 0, y: 2)
-                            .padding(.bottom, 10)
-                            .overlay(
-                                Text("Resposta 1")
-                                    .font(.custom("JockeyOne-Regular", size:22))
-                                    .foregroundColor(.white)
-                                    .padding(.bottom))
-                        Rectangle()
-                            .frame(width: 200, height: 40)
-                            .foregroundColor(Marrom)
-                            .shadow(color: Color.black, radius: 2, x: 0, y: 2)
-                            .padding(.bottom, 10)
-                            .overlay(
-                                Text("Resposta 2")
-                                    .font(.custom("JockeyOne-Regular", size:22))
-                                    .foregroundColor(.white)
-                                    .padding(.bottom))
-                        Rectangle()
-                            .frame(width: 200, height: 40)
-                            .foregroundColor(Marrom)
-                            .shadow(color: Color.black, radius: 2, x: 0, y: 2)
-                            .padding(.bottom, 10)
-                            .overlay(
-                                Text("Resposta 3")
-                                    .font(.custom("JockeyOne-Regular", size:22))
-                                    .foregroundColor(.white)
-                                    .padding(.bottom))
-                        Rectangle()
-                            .frame(width: 200, height: 40)
-                            .foregroundColor(Marrom)
-                            .shadow(color: Color.black, radius: 2, x: 0, y: 2)
-                            .padding(.bottom, 10)
-                            .overlay(
-                                Text("Resposta 4")
-                                    .font(.custom("JockeyOne-Regular", size:22))
-                                    .foregroundColor(.white)
-                                    .padding(.bottom))
-                        Spacer()
-                    }
+                        }  .ignoresSafeArea()
                     
+                }
+                Spacer()
+                VStack {
+                    Rectangle()
+                        .frame(width: 230, height:230)
                     Spacer()
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor?")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                    Rectangle()
+                        .frame(width: 200, height: 40)
+                        .foregroundColor(marrom)
+                        .shadow(color: Color.black, radius: 2, x: 0, y: 2)
+                        .padding(.bottom, 10)
+                        .overlay(
+                            Text("Resposta 1")
+                                .font(.custom("JockeyOne-Regular", size:22))
+                                .foregroundColor(.white)
+                                .padding(.bottom))
+                    Rectangle()
+                        .frame(width: 200, height: 40)
+                        .foregroundColor(marrom)
+                        .shadow(color: Color.black, radius: 2, x: 0, y: 2)
+                        .padding(.bottom, 10)
+                        .overlay(
+                            Text("Resposta 2")
+                                .font(.custom("JockeyOne-Regular", size:22))
+                                .foregroundColor(.white)
+                                .padding(.bottom))
+                    Rectangle()
+                        .frame(width: 200, height: 40)
+                        .foregroundColor(marrom)
+                        .shadow(color: Color.black, radius: 2, x: 0, y: 2)
+                        .padding(.bottom, 10)
+                        .overlay(
+                            Text("Resposta 3")
+                                .font(.custom("JockeyOne-Regular", size:22))
+                                .foregroundColor(.white)
+                                .padding(.bottom))
+                    Rectangle()
+                        .frame(width: 200, height: 40)
+                        .foregroundColor(marrom)
+                        .shadow(color: Color.black, radius: 2, x: 0, y: 2)
+                        .padding(.bottom, 10)
+                        .overlay(
+                            Text("Resposta 4")
+                                .font(.custom("JockeyOne-Regular", size:22))
+                                .foregroundColor(.white)
+                                .padding(.bottom))
+                    Spacer()
+                }
             }
+            Spacer()
         }
     }
+}
 struct paginaJogo_Previews: PreviewProvider {
     static var previews: some View {
         paginaJogo()
